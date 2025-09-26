1239d39c75997b577c55e7cef6a698010a987904
3DSilent <232480925+3DSilent@users.noreply.github.com>
2025-09-26 23:05:49 +0100

    comlete 09 removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(arr,val) {
+    val = Array.from(arguments);
+    val.shift();
+    for (let numb of val) {
+        while (arr.indexOf(numb) !== -1) {
+            arr.map(item =>{
+            if (item === numb) {arr.splice(arr.indexOf(item),1)} ;
+            })
+        }
+    } 
+    return arr;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
