964b253b87cce7a1450cf4cbec4c5e0dfbcd16e6
3DSilent <232480925+3DSilent@users.noreply.github.com>
2025-09-26 16:06:42 +0100

    Complete 07 reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversed = "";
+    for (i = 0 ; i <= str.length ; i++) {
+        reversed = str.charAt(i) + reversed;
+    }
+    return reversed;
 };
 
 // Do not edit below this line
