f7ed404b89ed0b0a4fce2a050210c424708b2267
3DSilent <232480925+3DSilent@users.noreply.github.com>
2025-09-26 15:50:14 +0100

    Complete 06-repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let repeatStr = str;
+    for (;num > 1;num--) {
+        repeatStr += str;
+    }
+    if (num ===0) {repeatStr = ""};
+    if (num < 0) {repeatStr = "ERROR"};
+    return repeatStr;
 };
 
 // Do not edit below this line
