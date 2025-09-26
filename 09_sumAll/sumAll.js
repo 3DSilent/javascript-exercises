1697e509dea40fa52a3ffb10c169317dc9db171e
3DSilent <232480925+3DSilent@users.noreply.github.com>
2025-09-26 23:32:08 +0100

    complete 09 sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    if (a < 0 || b < 0) { return "ERROR"};
+    // if (Math.floor(a) !== a || Math.floor(b) !== b ) {return "ERROR"};
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {return "ERROR"};
+    if (b - a > 0 ) {
+        for (;a <= b; a++){sum += a};
+    } else { 
+        for (;b <= a; b++){sum += b};
+    }
+    return sum;
 };
 
 // Do not edit below this line
